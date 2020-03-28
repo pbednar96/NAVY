58d1d8f9f9fe048617062635bb40b83dd8e2d63f
petr <petan.bednar@gmail.com>
2020-03-28 15:47:09 +0100

    added stack + comments

--- l_system.py
+++ l_system.py
@@ -1,11 +1,12 @@
-from math import pi
-
 import turtle
 
-ACTION = 3
-
+ACTION = 4
+ITER = 3
+LENGTH = 10
 
 def lsystem(axiom, rules, iterations):
+    #create final axiom
+
     for _ in range(iterations):
         final_axioms = ''
         for item in axiom:
@@ -18,17 +19,30 @@ def lsystem(axiom, rules, iterations):
 
 
 def turtle_route(axiom, angel):
+    # draw axiom with turtle
+
     window = turtle.Screen()
     window.title("L system")
     line = turtle.Turtle()
+    stack = []
+
+    line.left(90)
+
     for item in axiom:
         if item == "F":
-            line.forward(10)
+            line.forward(LENGTH)
         elif item == "+":
             line.right(angel)
         elif item == "-":
             line.left(angel)
-
+        elif item == '[':
+            stack.append((line.heading(), line.pos()))
+        elif item == ']':
+            heading, position = stack.pop()
+            line.penup()
+            line.goto(position)
+            line.setheading(heading)
+            line.pendown()
     turtle.done()
 
 
@@ -37,18 +51,30 @@ def main():
         axiom = "F+F+F+F"
         rules = "F+F-F-FF+F+F-F"
         angle = 90
-        final_axiom = lsystem(axiom, rules, 2)
-        print(final_axiom)
-        turtle_route(final_axiom, angle)
 
     if ACTION == 2:
         axiom = "F++F++F"
         rules = "F+F--F+F"
         angle = 60
-        final_axiom = lsystem(axiom, rules, 3)
-        print(final_axiom)
-        turtle_route(final_axiom, angle)
 
+    if ACTION == 3:
+        axiom = "F"
+        rules = "F[+F]F[-F]F"
+        angle = 180 / 7
+
+    if ACTION == 4:
+        axiom = "F"
+        rules = "FF+[+F-F-F]-[-F+F+F]"
+        angle = 180/8
+
+    if ACTION == 5:
+        axiom = "F"
+        rules = "F[+FF][-FF]F[-F][+F]F"
+        angle = 35
+
+    final_axiom = lsystem(axiom, rules, ITER)
+    print(final_axiom)
+    turtle_route(final_axiom, angle)
 
 if __name__ == "__main__":
     main()
